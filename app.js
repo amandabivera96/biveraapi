078ff401d4adbc22b6999dfea87135130fd91de4
amandabivera96 <amandabivera@gmail.com>
2021-02-28 20:31:37 +0530

    Api

--- app.js
+++ app.js
@@ -3,58 +3,51 @@ const app = express();
 const port = process.env.PORT||9000;
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const multer = require('multer');
-
-const path = require('path');
 const mongo = require('mongodb');
 const MongoClient = mongo.MongoClient;
 const mongourl="mongodb+srv://amandabivera:ammu123@cluster0.g2iwr.mongodb.net/biveraphotography?retryWrites=true&w=majority";
 let db;
 col_name1="categories";
-col_name2="wedding";
-col_name3="engagement";
+col_name2="features";
 
 app.use(cors());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
-
-const storage = multer.diskStorage({
-    destination:'./upload/images',
-    filename: (req,file,cb) =>{
-        return cb(null,`${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
-    }
-})
-
-const upload = multer({
-    storage: storage
-})
 
 //health check
 app.get("/",(req,res ) => {
     res.send('Health OK');
 })
 
-app.use('/cateimg',express.static('upload/images'))
-
 //To add Categories
-app.post('/addcategories',upload.single('cateimg'),(req,res) => {
+app.post('/addcategories',(req,res) => {
     console.log(req.file);
     db.collection(col_name1).insert(req.body,(err,result) => {
         if(err) throw err;
-        res.json({
-            success:1,
-            cateimg_url:`http://biveraapi.herokuapp.com/cateimg/${req.file.filename}`
-        })
+        res.send(result);
     })
 });
 
 //categories route
 app.get('/categories',(req,res)=>{
-    db.collection('categories').find().toArray((err,result)=>{
+    var condition={};
+    //get category on basis of catename
+    if(req.query.catename){
+        condition = {"catename":req.query.catename}
+    }
+    db.collection('categories').find(condition).toArray((err,result)=>{
         if(err) throw err;
         res.send(result);
     });
 });
+
+//features route
+app.get('/features',(req,res) => {
+    db.collection('features').find().toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result);
+    })
+})
 
 //connection with mongo server
 MongoClient.connect(mongourl,(err,connection) => {
